2dde69b6e3601ff0102f4569a599f58dc889b702
Fadli2001 <77084820+Fadli2001@users.noreply.github.com>
2021-01-19 09:55:29 +0700

    Update helloworld.java
    Menambah kodingan

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
   public static void main(){
     System.out.println("Hello World");
         System.out.println("Hello dunia tipu-tipu");
+         System.out.println("Assalamu'alaikum");
   }
 }
